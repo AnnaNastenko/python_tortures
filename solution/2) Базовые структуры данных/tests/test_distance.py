2fc0da6470ebc2aca2b4adec8929cbac977a80d5
Andrei Horbach <andrewhorbach@gmail.com>
2021-02-28 13:08:18 +0300

    Module 2. Dictionary compare for distance

--- solution/2) Базовые структуры данных/tests/test_distance.py
+++ solution/2) Базовые структуры данных/tests/test_distance.py
@@ -6,10 +6,10 @@ import unittest
 
 d = os.path.abspath(os.path.curdir)
 sys.path.append(d)
-from distance import moscow_london, moscow_paris, london_moscow, london_paris, paris_moscow, paris_london
+from distance import moscow_london, moscow_paris, london_moscow, london_paris, paris_moscow, paris_london, distances
 
 
-class TestFavMovie(unittest.TestCase):
+class TestDist(unittest.TestCase):
     def setUp(self):
         # M
         self.iml, self.imp = 145.60219778561037, 130.38404810405297
@@ -25,6 +25,10 @@ class TestFavMovie(unittest.TestCase):
         self.lp = london_paris
         self.pl = paris_london
 
+        self.d = {'London': {'Moscow': 145.6, 'Paris': 42.43},
+                  'Moscow': {'London': 145.6, 'Paris': 130.38},
+                  'Paris': {'London': 42.43, 'Moscow': 130.38}}
+
     def test_moscow(self):
         self.assertAlmostEqual(self.iml, self.ml, msg="Москва <-> Лондон", places=1)
         self.assertAlmostEqual(self.imp, self.mp, msg="Москва <-> Париж", places=1)
@@ -37,27 +41,12 @@ class TestFavMovie(unittest.TestCase):
         self.assertAlmostEqual(self.imp, self.pm, msg="Париж <-> Москва", places=1)
         self.assertAlmostEqual(self.ilp, self.pl, msg="Париж <-> Лондон", places=1)
 
+    def test_dictionary(self):
+        self.assertDictEqual(distances, self.d, msg="Сравнение словарей")
+
 
 if __name__ == "__main__":
     unittest.main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 class TestFavMovie(unittest.TestCase):
--- solution/3) Основные операторы/01_days_in_month.py
+++ solution/3) Основные операторы/01_days_in_month.py
@@ -9,6 +9,28 @@
 # Номер месяца получать от пользователя следующим образом
 user_input = input("Введите, пожалуйста, номер месяца: ")
 month = int(user_input)
-print('Вы ввели', month)
 
 # TODO здесь ваш код
+
+days_in_each_month = {1: [31, 'January'],
+                      2: [28, 'February'],
+                      3: [31, 'March'],
+                      4: [30, 'April'],
+                      5: [31, 'May'],
+                      6: [30, 'June'],
+                      7: [31, 'Jule'],
+                      8: [31, 'August'],
+                      9: [30, 'September'],
+                      10: [31, 'October'],
+                      11: [30, 'November'],
+                      12: [31, 'December']
+                      }
+
+print(f'''Вы ввели {month} "{days_in_each_month[month][1]}, в нем"''', end=' ')
+
+
+if month in days_in_each_month:
+    dm = days_in_each_month[month][0]
+    print(dm)
+else:
+    print('Введен некорректный номер месяца')
